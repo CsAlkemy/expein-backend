3ef5d4627448501d1061df4a9821dc0d2dda2248
CsAlkemy <alkemy@frontpayment.no>
2023-09-21 19:55:15 +0600

    swagger api doc added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Expein API Documentation')
+    .setDescription('API documentation of Expein Financial tracker')
+    .setVersion('0.1')
+    .addTag('User Login')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('apiDoc', app, document);
+  await app.listen(3001);
 }
 bootstrap();
